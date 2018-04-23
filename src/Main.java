ad46f84049c74abbe3f7d07bed3262de87d59a94
Flavio <f.giordani003@studenti.unibs.it>
2018-04-23 18:22:00 +0200

    update main(work in progress

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
     private static final String MESSAGGIO_COORDINATE = "Inserisca le coordinate ";
     private static final String MESSAGGIO_RIMOZIONE= "Inserisca il nome del pianeta da rimuovere";
     private static final String MESSAGGIO_RIMOZIONE_COMPLETATA = "Il corpo celeste è stato rimosso con successo";
+    private static final String AGGIUNTA_RIMOZIONE = "Aggiungere/Rimuovere corpo celeste";
 
     private static final int MIN_MASSA = 0;
 
@@ -24,18 +25,23 @@ public class Main {
     private static final String LUNE_ORBITANTI = "Visualizzazione lune orbitanti intorno da un pianeta";
     private static final String CALCOLO_CENTRO= "Calcolo del centro di massa di %s";
     private static final String CALCOLO_PERCORSO = "Calcolo percorso verso una luna";
+    private static final String MESSAGGIO_RIMOZIONE_FALLITO ="Operazione fallita: corpo celeste non trovato";
 
     private static final String[] OPZIONI_MENU = {
-            AGGIUNTA_PIANETA, AGGIUNTA_LUNA,
-            RIMOZIONE_PIANETA, RIMOZIONE_LUNA,
             RICERCA_CORPO_CELESTE, LUNE_ORBITANTI,
             CALCOLO_CENTRO, CALCOLO_PERCORSO
+            };
+    private static final String[] OPZIONI_MENU_ADD_REMOVE = {
+            AGGIUNTA_RIMOZIONE,
+            AGGIUNTA_PIANETA,AGGIUNTA_LUNA,
+            RIMOZIONE_PIANETA,RIMOZIONE_LUNA
             };
 
     public static void main(String[] args) {
         int scelta;
         String nomeSistema;
-        MyMenu menu = new MyMenu(TITOLO_MENU,OPZIONI_MENU);
+        MyMenu menu = new MyMenu(TITOLO_MENU, OPZIONI_MENU);
+        MyMenu menuAddRemove = new MyMenu(TITOLO_MENU, OPZIONI_MENU_ADD_REMOVE);
         SistemaStellare sistema = init();
         nomeSistema = sistema.nomeSistema();
 
@@ -43,23 +49,32 @@ public class Main {
             scelta = menu.scegli();
             switch (scelta){
                 case 1:
-                    sistema.addPianeta(initPianeta());
+                    switch (menuAddRemove.scegli()){
+                        case 1:
+                            sistema.addPianeta(initPianeta());
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            if(sistema.removePianeta(InputDati.leggiStringa(MESSAGGIO_RIMOZIONE)))
+                                System.out.println(MESSAGGIO_RIMOZIONE_COMPLETATA);
+                            else
+                                System.out.println(MESSAGGIO_RIMOZIONE_FALLITO);
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 case 2:
+
                     break;
                 case 3:
-                    sistema.removePianeta(InputDati.leggiStringa(MESSAGGIO_RIMOZIONE));
-                    System.out.println(MESSAGGIO_RIMOZIONE_COMPLETATA);
-                    break;
+
                 case 4:
                     break;
                 case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
                     break;
                 default:
                     break;
@@ -88,5 +103,20 @@ public class Main {
         y = tastiera.nextDouble();
         posizone = new Punto(x, y, nome);
         return new Pianeta(nome,massa,posizone);
+    }
+
+    private static Luna initLuna(){
+        String nome;
+        double massa, x, y;
+        Punto posizone;
+
+        Scanner tastiera = new Scanner(System.in);
+        nome = InputDati.leggiStringa(NOME_CORPO_CELESTE);
+        massa = InputDati.leggiDoubleConMinimo(String.format(INSERIMENTO_MASSA, nome), MIN_MASSA);
+        System.out.print(MESSAGGIO_COORDINATE);
+        x = tastiera.nextDouble();
+        y = tastiera.nextDouble();
+        posizone = new Punto(x, y, nome);
+        return new Luna(nome,massa,posizone);
     }
 }
